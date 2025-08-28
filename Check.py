79ee6be0fd3f5b1e0326a4319abd8dd0b9716f64
caesarion mikhael <caesarion.mikhael@luminestudio.net>
2025-08-29 00:32:57 +0700

    second commit

---
+++ Check.py
@@ -0,0 +1,16 @@
+def string_appender(name, append_str):
+    return "%s_%s" % (name, append_str) 
+
+def name__():
+    if __name__ == "__main__":
+        name_list = ["kenny", "hippo", "peter", "kelly", "john", "bob"]
+        funcs = []
+        for n in name_list:
+            funcs.append(lambda n=n: string_appender(n, "char"))
+
+        # Don’t modify the code below:
+        for f in funcs:
+            print(f())
+
+name__()
+
